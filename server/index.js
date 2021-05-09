86c770a689fbc6b908ccc0e6b2e65324072c5e48
erenttss <erentutus56@gmail.com>
2021-05-10 00:17:46 +0300

    Books GET and POST implementation

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const db = require("./models");
+
+// Routers
+
+const bookRouter = require("./routes/Books");
+app.use("/books", bookRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
---
+++ server/routes/Books.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { Books } = require("../models");
+
+router.get("/", async (req, res) => {
+  const listOfBooks = await Books.findAll(); // findAll : sequalize method
+  res.json(listOfBooks);
+});
+
+router.post("/", async (req, res) => {
+  const book = req.body;
+  await Books.create(book);
+  res.json(book);
+});
+
+module.exports = router;
