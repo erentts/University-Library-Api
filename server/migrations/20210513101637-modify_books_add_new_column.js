466474b26a97c1902246ffdb34cb73f5dc9640b2
erenttss <erentutus56@gmail.com>
2021-05-13 13:37:06 +0300

    migrated Books Table

---
+++ server/migrations/20210513101637-modify_books_add_new_column.js
@@ -0,0 +1,50 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addColumn("Books", "materialType", {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+    await queryInterface.addColumn("Books", "isAvailable", {
+      type: Sequelize.TINYINT,
+      allowNull: false,
+    });
+    await queryInterface.addColumn("Books", "isReservation", {
+      type: Sequelize.TINYINT,
+      allowNull: false,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
+
+/* 
+  materialType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reservationState: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+    },
+    isAvailable: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+    },
+
+*/
